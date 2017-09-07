3313b72917fca458d17a713a87a7d945d5f7bc0d
nathan-beneke <benek020@morris.umn.edu>
2017-09-07 17:02:43 -0500

    Finish bin/create_username_dist.sh and add documentation to all scripts Clean up some useless files

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,11 +2,13 @@
 
 cd $1
 
+#Places all attempts on invalid users into a file
 cat var/log/* | grep -e \^\.\*"Failed password"\.\*\$ | grep -e \^\.\*"invalid user"\.\*\$ | awk -F" " '{print $1 " " $2 " " $3 " " $11 " " $13}' | awk -F":" '{print $1 " " $3}' | awk -F" " '{print $1 " " $2 " " $3 " " $5 " " $6}' > tmpFile.txt
-#This works for all attempts with invalid users, but not with valid users
 
+#Places all attempts with valid users, but wrong passwords into a different file
 cat var/log/* | grep -e \^\.\*"Failed password"\.\*\$ | grep -ve \.\*"invalid user"\.\*\$ | awk -F" " '{print $1 " " $2 " " $3 " " $9 " " $11}' | awk -F":" '{print $1 " " $3}' | awk -F" " '{print $1 " " $2 " " $3 " " $5 " " $6}' > tmpFile2.txt
 
+#concatenates the two files into the final, desired one
 cat tmpFile.txt tmpFile2.txt > failed_login_data.txt
 
 rm tmpFile.txt tmpFile2.txt
