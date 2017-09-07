e2dc57f6be27d3a1f430ae17cc9b12a1e5f952a5
nathan-beneke <benek020@morris.umn.edu>
2017-09-07 11:51:13 -0500

    make progress towards creating the username distribution

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+cat $1/*/failed_login_data.txt | awk -F" " '{print $4}' > tmpFile1.txt
+# gets list of users
+sort tmpFile1 | uniq -c | data.addRow([$2, $1])sort tmpFile1 | uniq -c | awk -F" " '{print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' > tmpFile2.txt
+# sorts the list, counts how many of each username is listed, turns it into "data.addRow(['web7', 16])" format, and dumps it in a tmp file
+
+./bin/wrap_contents.sh tmpFile2.txt html_components/username_dist $1/username_dist.html
+
+
+
+
 
